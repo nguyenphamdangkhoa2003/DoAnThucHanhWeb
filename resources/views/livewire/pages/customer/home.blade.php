a23102d22b1df5f61092617bbb41bb7213e65de9
quikiet <quikiet03@gmail.com>
2024-12-16 18:46:27 +0700

    chỉnh sửa card room lần 3 ở hiển thị children adults

--- resources/views/livewire/pages/customer/home.blade.php
+++ resources/views/livewire/pages/customer/home.blade.php
@@ -69,11 +69,15 @@
                                                                         </p>
                                                             <p class="text-sm text-gray-400 truncate max-w-sm">{{ $type_room->description }}</p>
                                                             <div class="flex flex-col justify-between">
-                                                                    <div class="flex">
-                                                                        <x-mary-icon name="o-user" class="me-3" />
+                                                                    <div class="flex gap-1 items-center">
+                                                                        <x-mary-icon name="o-user" class="" />
                                                                         @php
-                                                                        echo $type_room->children + $type_room->adults;
-                                                                        @endphp Guest
+                                                                        echo $type_room->adults;
+                                                                        @endphp  <span  class="pr-1 border-r">Adults</span>
+                                                                        <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="#000000" viewBox="0 0 256 256"><path d="M92,140a12,12,0,1,1,12-12A12,12,0,0,1,92,140Zm72-24a12,12,0,1,0,12,12A12,12,0,0,0,164,116Zm-12.27,45.23a45,45,0,0,1-47.46,0,8,8,0,0,0-8.54,13.54,61,61,0,0,0,64.54,0,8,8,0,0,0-8.54-13.54ZM232,128A104,104,0,1,1,128,24,104.11,104.11,0,0,1,232,128Zm-16,0a88.11,88.11,0,0,0-84.09-87.91C120.32,56.38,120,71.88,120,72a8,8,0,0,0,16,0,8,8,0,0,1,16,0,24,24,0,0,1-48,0c0-.73.13-14.3,8.46-30.63A88,88,0,1,0,216,128Z"></path></svg>
+                                                                        @php
+                                                                        echo $type_room->children;
+                                                                        @endphp Childrens
                                                                     </div>
                                                                     <div class="flex text-green-600 font-semibold">
                                                                         {{ $room_available }}
